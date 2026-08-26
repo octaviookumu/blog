b45023ecdc130f8d5e2f6443f64ede804772c57a
octaviookumu <octaviookumu@gmail.com>
2026-08-26 16:34:45 +0300

    feat: configure category-post relationship

--- backend/src/main/java/com/octaviookumu/blog/domain/entities/Category.java
+++ backend/src/main/java/com/octaviookumu/blog/domain/entities/Category.java
@@ -3,6 +3,8 @@ package com.octaviookumu.blog.domain.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -21,6 +23,10 @@ public class Category {
 
     @Column(nullable = false, unique = true)
     private String name;
+
+    @OneToMany(mappedBy = "category") // no cascades needed as category and post have different life cycles.
+    // so deleting a category should be prevented if it has posts
+    private List<Post> posts = new ArrayList<>();
 
     @Override
     public boolean equals(Object o) {
