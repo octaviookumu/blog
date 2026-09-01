43e510ef40b089cf2766a8a7c7eda64a159d0d85
octaviookumu <octaviookumu@gmail.com>
2026-09-01 19:37:45 +0300

    feat: JWT Authentication Filter Part 2 - Log in with a user - Configure our filter after allowing /auth requests - This is a VERY BASIC implementation and you'll want to do more in a proper production system

--- backend/src/main/java/com/octaviookumu/blog/config/SecurityConfig.java
+++ backend/src/main/java/com/octaviookumu/blog/config/SecurityConfig.java
@@ -1,5 +1,10 @@
 package com.octaviookumu.blog.config;
 
+import com.octaviookumu.blog.domain.entities.User;
+import com.octaviookumu.blog.repositories.UserRepository;
+import com.octaviookumu.blog.security.BlogUserDetailsService;
+import com.octaviookumu.blog.security.JwtAuthenticationFilter;
+import com.octaviookumu.blog.services.AuthenticationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,17 +13,50 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public JwtAuthenticationFilter jwtAuthenticationFilter(AuthenticationService authenticationService) {
+        return new JwtAuthenticationFilter(authenticationService);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        BlogUserDetailsService blogUserDetailsService = new BlogUserDetailsService(userRepository);
+
+        String email = "olivia@test.com";
+        userRepository.findByEmail(email).orElseGet(() -> {
+            // Create the user if they don't exist
+            // (Not something you should do in a production environment)
+            // You want to build out that full user management functionality
+            User newUser = User.builder()
+                    .name("Test User")
+                    .email(email)
+                    .password(passwordEncoder().encode("password"))
+                    .build();
+
+            // in this way we will create a test user to use if that user doesn't exist in the database
+            return userRepository.save(newUser);
+        });
+
+
+        return blogUserDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception { // spring will know to inject jwtAuthenticationFilter as we have specified the bean above
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/posts/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/categories/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/tags/**").permitAll()
@@ -27,7 +65,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable) // disable the csrf tokens, won't use them
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // using stateless authentication
-                );
+                ).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
