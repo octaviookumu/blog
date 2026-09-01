425495a222525bb471b820e67915149ea4b75b44
octaviookumu <octaviookumu@gmail.com>
2026-09-01 17:28:57 +0300

    feat: authenticate endpoint part 1 - starting with implementing the controller layer first to see how it is used

--- backend/src/main/java/com/octaviookumu/blog/controllers/ErrorController.java
+++ backend/src/main/java/com/octaviookumu/blog/controllers/ErrorController.java
@@ -64,4 +64,6 @@ public class ErrorController {
         return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
     }
 
+
+
 }
---
+++ backend/src/main/java/com/octaviookumu/blog/domain/dtos/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.octaviookumu.blog.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthResponse {
+    // this information is baked into the token itself, but it's always useful to have it available easily
+    private String token;
+    private long expiresIn;
+}
